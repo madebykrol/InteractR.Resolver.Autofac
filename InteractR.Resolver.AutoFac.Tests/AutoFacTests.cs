9ce3d77183277a75338c03c76a487b86fd1ac325
Kristoffer Olsson <kristoffer.olsson@cgi.com>
2019-11-08 21:18:14 +0100

    Initial Commit.

---
+++ InteractR.Resolver.AutoFac.Tests/AutoFacTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using InteractorHub.Interactor;
+using InteractorHub.Notification;
+using InteractorHub.Resolvers.AutoFac;
+using InteractorHub.Tests.Mocks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace InteractorHub.Tests.Resolvers.AutoFac
+{
+    [TestFixture]
+    public class AutoFacTests
+    {
+        private IContainer _container;
+        private IInteractorHub _hub;
+        private IInteractor<MockInteractionRequest, MockResponse> _useCaseInteractor;
+        private INotificationListener<MockNotification> _notificationListener1;
+        private INotificationListener<MockNotification> _notificationListener2;
+        private INotificationListener<MockNotification> _notificationListener3;
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new ContainerBuilder();
+            _useCaseInteractor = Substitute.For<IInteractor<MockInteractionRequest, MockResponse>>();
+            _notificationListener1 = Substitute.For<INotificationListener<MockNotification>>();
+            _notificationListener2 = Substitute.For<INotificationListener<MockNotification>>();
+            _notificationListener3 = Substitute.For<INotificationListener<MockNotification>>();
+
+            builder.RegisterInstance(_useCaseInteractor).As<IInteractor<MockInteractionRequest, MockResponse>>();
+            builder.RegisterInstance(_notificationListener1).As<INotificationListener<MockNotification>>();
+            builder.RegisterInstance(_notificationListener2).As<INotificationListener<MockNotification>>();
+
+            _container = builder.Build();
+
+            _hub = new Hub(new AutoFacResolver(_container));
+        }
+
+        [Test]
+        public async Task Test_AutoFac_Resolver()
+        {
+            await _hub.Handle<MockResponse, MockInteractionRequest>(new MockInteractionRequest());
+            await _useCaseInteractor.Received().Handle(Arg.Any<MockInteractionRequest>(), Arg.Any<CancellationToken>());
+        }
+
+        [Test]
+        public async Task Test_AutoFac_NotificationListeners()
+        {
+            await _hub.Send(new MockNotification());
+            await _notificationListener1.Received().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
+            await _notificationListener2.Received().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
+        }
+
+        [Test]
+        public async Task Test_AutoFac_NotificationListener_NotCalled()
+        {
+            await _hub.Send(new MockNotification());
+            await _notificationListener3.DidNotReceive().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
+        }
+    }
+}
---
+++ InteractR.Resolver.AutoFac/AutoFacResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using InteractorHub.Resolver;
+
+namespace InteractorHub.Resolvers.AutoFac
+{
+    public class AutoFacResolver : IResolver
+    {
+        private readonly IContainer _container;
+        public AutoFacResolver(IContainer container)
+        {
+            _container = container;
+        }
+        public TInteractor ResolveInteractor<TInteractor>()
+        {
+            return Resolve<TInteractor>();
+        }
+
+        public object ResolveInteractor(Type interactorType)
+        {
+            return Resolve(interactorType);
+        }
+
+        private T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        private object Resolve(Type t)
+        {
+            return _container.Resolve(t);
+        }
+    }
+}
