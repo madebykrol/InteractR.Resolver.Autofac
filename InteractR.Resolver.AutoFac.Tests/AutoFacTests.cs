2d6b1fd64687b2a1679e495c91a252ea0121125d
Kristoffer Olsson <madebykrol@gmail.com>
2019-12-30 21:12:13 +0100

    Updated to latest InteractR version. Added Middleware support. Bumped Version Number.

--- InteractR.Resolver.AutoFac.Tests/AutoFacTests.cs
+++ InteractR.Resolver.AutoFac.Tests/AutoFacTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using InteractorHub.Resolvers.AutoFac;
@@ -17,18 +18,46 @@ namespace InteractorHub.Tests.Resolvers.AutoFac
         private IInteractorHub _interactorHub;
         private IInteractor<MockUseCase, IMockOutputPort> _useCaseInteractor;
 
+        private IMiddleware<MockUseCase, IMockOutputPort> _middleware1;
+        private IMiddleware<MockUseCase, IMockOutputPort> _middleware2;
+
+
 
         [SetUp]
         public void Setup()
         {
             var builder = new ContainerBuilder();
             _useCaseInteractor = Substitute.For<IInteractor<MockUseCase, IMockOutputPort>>();
+            _middleware1 = Substitute.For<IMiddleware<MockUseCase, IMockOutputPort>>();
+            _middleware1.Execute(
+                    Arg.Any<MockUseCase>(),
+                    Arg.Any<IMockOutputPort>(),
+                    d => Task.FromResult(new UseCaseResult(true)),
+                    Arg.Any<CancellationToken>())
+                .ReturnsForAnyArgs(x => new UseCaseResult(true))
+                .AndDoes(x => x.Arg<Func<MockUseCase, Task<UseCaseResult>>>().Invoke(x.Arg<MockUseCase>()));
+
+            _middleware2 = Substitute.For<IMiddleware<MockUseCase, IMockOutputPort>>();
+            _middleware2.Execute(
+                    Arg.Any<MockUseCase>(),
+                    Arg.Any<IMockOutputPort>(),
+                    d => Task.FromResult(new UseCaseResult(true)),
+                    Arg.Any<CancellationToken>())
+                .ReturnsForAnyArgs(x => new UseCaseResult(true))
+                .AndDoes(x => x.Arg<Func<MockUseCase, Task<UseCaseResult>>>().Invoke(x.Arg<MockUseCase>()));
+
 
             builder.RegisterInstance(_useCaseInteractor).As<IInteractor<MockUseCase, IMockOutputPort>>();
+
+            builder.RegisterInstance(_middleware1)
+                .As<IMiddleware<MockUseCase, IMockOutputPort>>();
+            builder.RegisterInstance(_middleware2)
+                .As<IMiddleware<MockUseCase, IMockOutputPort>>();
 
             _container = builder.Build();
 
             _interactorHub = new Hub(new AutoFacResolver(_container));
+
         }
 
         [Test]
@@ -38,5 +67,12 @@ namespace InteractorHub.Tests.Resolvers.AutoFac
             await _useCaseInteractor.Received().Execute(Arg.Any<MockUseCase>(), Arg.Any<IMockOutputPort>(), Arg.Any<CancellationToken>());
         }
 
+        [Test]
+        public async Task Test_Pipeline()
+        {
+            await _interactorHub.Execute(new MockUseCase(), (IMockOutputPort)new MockOutputPort());
+            await _middleware2.ReceivedWithAnyArgs().Execute(Arg.Any<MockUseCase>(), Arg.Any<IMockOutputPort>(), Arg.Any<Func<MockUseCase, Task<UseCaseResult>>>(),
+                Arg.Any<CancellationToken>());
+        }
     }
 }
--- InteractR.Resolver.AutoFac/AutoFacResolver.cs
+++ InteractR.Resolver.AutoFac/AutoFacResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Autofac;
 using InteractR.Interactor;
 using InteractR.Resolver;
@@ -21,6 +22,11 @@ namespace InteractorHub.Resolvers.AutoFac
         public IInteractor<TUseCase, TOutputPort> ResolveInteractor<TUseCase, TOutputPort>(TUseCase useCase) where TUseCase : IUseCase<TOutputPort>
         {
             return Resolve<IInteractor<TUseCase, TOutputPort>>();
+        }
+
+        public IReadOnlyList<IMiddleware<TUseCase, TOutputPort>> ResolveMiddleware<TUseCase, TOutputPort>(TUseCase useCase) where TUseCase : IUseCase<TOutputPort>
+        {
+            return _container.Resolve<IEnumerable<IMiddleware<TUseCase, TOutputPort>>>().ToList();
         }
     }
 }
